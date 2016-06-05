29f4627097e0c17ccbc1ee12e019a02e4cd42d3a
矢野史洋 <a2.mj.pop@gmail.com>
2016-06-05 21:50:46 +0900

    delete ストーリーボード削除

---
+++ SampleSearchItunes/ItunesConnection.swift
@@ -0,0 +1,16 @@
+//
+//  ItunesConnection.swift
+//  SampleSearchItunes
+//
+//  Created by 矢野史洋 on 2016/06/05.
+//  Copyright © 2016年 矢野史洋. All rights reserved.
+//
+
+import UIKit
+
+class ItunesConnection: NSObject {
+    
+    class func getAlbumForString(searchString:String){
+       //
+    }
+}
